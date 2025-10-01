8a7bc23da5d5d38d8ceaf44964113efbc0302944
Dc-4nderson <andersondequan5@gmail.com>
2025-10-01 09:22:13 -0400

    base for webapp

---
+++ app.py
@@ -0,0 +1,9 @@
+#import dependencies
+#import functions
+
+#init app
+#/home
+#a database area to show all checkins which get pulled from my JSON file
+#rag chatbot pulling context from my pinecone vector database
+#/upload checkins 
+#coming soon (need slack auth)
