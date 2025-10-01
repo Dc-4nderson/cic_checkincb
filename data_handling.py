9471cb87fb02393f8fceaf03423c8e40950f156a
Dc-4nderson <andersondequan5@gmail.com>
2025-10-01 09:28:25 -0400

    Add initial structure for data handling functions

---
+++ data_handling.py
@@ -0,0 +1,8 @@
+#import dependencies
+# def pc_init():
+  #init pinecone
+#pc_init()
+#upsert data to my pinecone instance using sentence transformers embed model or open ai
+# def show_checkins(data)
+  #call in my json file
+#return my json file
